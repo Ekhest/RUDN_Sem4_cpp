8148dba6f499be8d691f8547e47c3c2d357f6768
Ekhest <132713811+Ekhest@users.noreply.github.com>
2024-02-27 23:08:40 +0300

    Add files via upload

---
+++ laba2/Matrix.cpp
@@ -0,0 +1,154 @@
+#include "Matrix.h"
+
+// Constructor for Matrix class, initializes matrix with dimensions m x n
+Matrix::Matrix(unsigned int m, unsigned int n) {
+    this->m = m;
+    this->n = n;
+    data = new int*[m];
+    for (unsigned int i = 0; i < m; i++) {
+        data[i] = new int[n];
+    }
+}
+
+// Destructor for Matrix class, deallocates memory for matrix
+Matrix::~Matrix() {
+    for (unsigned int i = 0; i < m; i++) {
+        delete[] data[i];
+    }
+    delete[] data;
+}
+
+int& Matrix::operator[](int i) {
+    return data[i / n][i % n];
+}
+
+void Matrix::fillRandom() {
+    srand(time(0));
+    for (unsigned int i = 0; i < m; i++) {
+        for (unsigned int j = 0; j < n; j++) {
+            data[i][j] = rand() % 10;
+        }
+    }
+}
+void Matrix::fillRandom1() {
+    srand(time(0));
+    for (unsigned int i = 0; i < m; i++) {
+        for (unsigned int j = 0; j < n; j++) {
+            data[i][j] = rand() % 11;
+        }
+    }
+}
+
+// Overloaded operator<< to output matrix to ostream
+std::ostream& operator<<(std::ostream& out, const Matrix& matrix) {
+    for (unsigned int i = 0; i < matrix.m; i++) {
+        for (unsigned int j = 0; j < matrix.n; j++) {
+            out << matrix.data[i][j] << " ";
+        }
+        out << std::endl;
+    }
+    return out;
+}
+
+Matrix& Matrix::operator+=(const Matrix& matrix) {
+    if (m != matrix.m || n != matrix.n) {
+        std::cerr << "Matrices must have the same dimensions" << std::endl;
+        return *this;
+    }
+    for (unsigned int i = 0; i < m; i++) {
+        for (unsigned int j = 0; j < n; j++) {
+            data[i][j] += matrix.data[i][j];
+        }
+    }
+    return *this;
+}
+
+Matrix Matrix::operator+(const Matrix& matrix) const {
+    Matrix result(m, n);
+    for (unsigned int i = 0; i < m; i++) {
+        for (unsigned int j = 0; j < n; j++) {
+            result.data[i][j] = data[i][j] + matrix.data[i][j];
+        }
+    }
+    return result;
+}
+
+Matrix& Matrix::operator-=(const Matrix& matrix) {
+    if (m != matrix.m || n != matrix.n) {
+        std::cerr << "Matrices must have the same dimensions" << std::endl;
+        return *this;
+    }
+    for (unsigned int i = 0; i < m; i++) {
+        for (unsigned int j = 0; j < n; j++) {
+            data[i][j] -= matrix.data[i][j];
+        }
+    }
+    return *this;
+}
+
+Matrix Matrix::operator-(const Matrix& matrix) const {
+    Matrix result(m, n);
+    for (unsigned int i = 0; i < m; i++) {
+        for (unsigned int j = 0; j < n; j++) {
+            result.data[i][j] = data[i][j] - matrix.data[i][j];
+        }
+    }
+    return result;
+}
+
+Matrix& Matrix::operator*=(const Matrix& matrix) {
+    if (n != matrix.m) {
+        std::cerr << "Matrices must have compatible dimensions for multiplication" << std::endl;
+        return *this;
+    }
+    Matrix result(m, matrix.n);
+    for (unsigned int i =  0; i < m; i++) {
+        for (unsigned int j =  0; j < matrix.n; j++) {
+            result.data[i][j] =  0;
+            for (unsigned int k =  0; k < n; k++) {
+                result.data[i][j] += data[i][k] * matrix.data[k][j];
+            }
+        }
+    }
+    *this = result;
+    return *this;
+}
+
+// Overloaded operator* to return a new matrix that is the product of this matrix and another matrix
+Matrix Matrix::operator*(const Matrix& matrix) const {
+    if (n != matrix.m) {
+        std::cerr << "Matrices must have compatible dimensions for multiplication" << std::endl; // Error message if dimensions mismatch
+        return Matrix(0,  0);
+    }
+    Matrix result(m, matrix.n);
+    for (unsigned int i =  0; i < m; i++) {
+        for (unsigned int j =  0; j < matrix.n; j++) {
+            result.data[i][j] =  0; 
+            for (unsigned int k =  0; k < n; k++) {
+                result.data[i][j] += data[i][k] * matrix.data[k][j];
+            }
+        }
+    }
+    return result;
+}
+
+// Overloaded operator!= to check if this matrix is not equal to another matrix
+bool Matrix::operator!=(const Matrix& matrix) const {
+    if (m != matrix.m || n != matrix.n) {
+        return true;
+    }
+    for (unsigned int i = 0; i < m; i++) {
+        for (unsigned int j = 0; j < n; j++) {
+            if (data[i][j] != matrix.data[i][j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+// Overloaded operator== to check if this matrix is equal to another matrix
+bool Matrix::operator==(const Matrix& matrix) const {
+    return !(*this != matrix);
+}
+
